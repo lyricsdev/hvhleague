8b5a29c9b9b90da3a791c930c3ad8312a0badd28
Алексей ага <lordmzc@icloud.com>
2023-12-14 16:06:29 +0300

    ast

--- frontend/app/components/inLobbyPlayerSkeleton.tsx
+++ frontend/app/components/inLobbyPlayerSkeleton.tsx
@@ -1,12 +1,25 @@
-import { Card, CardBody, Image, Skeleton } from "@nextui-org/react";
-
-const SkeletonLobbyPlayer: React.FC = ({  }) => {    return (
+import { Button, Card, CardBody, Image, Skeleton } from "@nextui-org/react";
+import { ActionFunction } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+export const  action : ActionFunction = async({ request, context }) => {
+    const formData = await request.formData();
+    console.log(formData)
+    return {
+        dada: "dada"
+    }
+}
+const SkeletonLobbyPlayer: React.FC = ({ }) => {
+    return (
         <Card>
             <CardBody>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                <Skeleton onClick={()=>{}} className="flex rounded-full w-10 h-10 pr-5"/>
-                    <Skeleton className="inline h-3 w-36 rounded-lg"/>
-                </div>
+                <Form method="POST">
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <button type="submit" name="action" value="login" className="flex rounded-full w-5 h-10 pr-5">
+
+                        </button>
+                        <Skeleton className="inline h-3 w-36 rounded-lg" />
+                    </div>
+                </Form>
             </CardBody>
         </Card>
     )
